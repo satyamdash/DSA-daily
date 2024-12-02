b4928ffbf039f5086fd6e3c4b58cbf23d63b9765
Satyam Gupta <satyamgupta@Satyams-MacBook-Air.local>
2024-12-02 19:30:21 +0530

    Problem 37

---
+++ 37-Sudoku-Solver.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board,int row,int col,char c)
+    {
+        for (int i = 0; i < 9; i++) {
+
+            if (board[row][i] == c || board[i][col] == c) {
+                return false;
+            }
+
+
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board)
+    {
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[0].size();j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(isValid(board,i,j,c))
+                        {
+                         board[i][j]=c;
+                        if(solve(board))
+                        {
+                            return true; 
+                        }
+
+                        board[i][j]='.';
+                    } 
+                }
+                return false;
+            }
+
+        } 
+    }
+    return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) 
+    {
+        solve(board);
+    }
+};
