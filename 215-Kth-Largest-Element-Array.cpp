91ca5d60f8c528479aab6a46fe6039b4bbe60f88
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-14 14:33:02 +0530

    Leetcode 215

---
+++ 215-Kth-Largest-Element-Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+      priority_queue<int> maxHeap;
+
+      for(int i=0;i<nums.size();i++)
+      {
+        maxHeap.push(nums[i]);
+      }  
+
+        for(int i=0;i<k-1;i++)
+        {
+            maxHeap.pop();
+        }
+
+      return maxHeap.top();
+    
+    }
+};
