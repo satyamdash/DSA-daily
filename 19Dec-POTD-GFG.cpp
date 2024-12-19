bab89d0b3f44ef46bd806ffd1c81b24290783164
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-19 23:16:44 +0530

    optimized

--- 19Dec-POTD-GFG.cpp
+++ 19Dec-POTD-GFG.cpp
@@ -2,21 +2,18 @@ class Solution {
   public:
     int kthMissing(vector<int> &arr, int k) 
     {
-        unordered_set<int> s(arr.begin(), arr.end());
-        
+
        int n=arr.size();
-       int count=0;
-       int curr=0;
-        while(count<k)
-        {
-            curr++;
-            if(s.find(curr)==s.end())
-            {
-                count++;
-            }
-            
-        }
-        return curr;
+       
+       for(int i=0;i<n;i++)
+       {
+           if(k+i<arr[i])
+           {
+               return k+i;
+           }
+       }
+       
+        return k+n;
     }
     
 };
