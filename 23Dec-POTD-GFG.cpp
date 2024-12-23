3e5edc5cacc1fbaf3f19d0481dade0e29b6cb10d
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-23 12:07:58 +0530

    Search in a sorted row matrix

---
+++ 23Dec-POTD-GFG.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchRowMatrix(vector<vector<int>> &mat, int x)
+    {
+        int n=mat.size();
+        int m=mat[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            int low=0;
+            int high=m-1;
+            while(low<=high)
+            {
+
+                int mid=(low+high)/2;   
+                
+                if(mat[i][mid]==x)
+                {
+                    return true;
+                }
+                else if(mat[i][mid]>x)
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
