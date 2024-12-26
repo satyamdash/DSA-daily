30c5d6713ef11f0641b0c6674004b30f86cfa4c9
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-26 11:58:02 +0530

    Two sum pair

---
+++ 26Dec-POTD-GFG.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) 
+    {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int i=0;
+        int j=n-1;
+        while(i<j)
+        {
+           int sum=arr[i]+arr[j];
+           if(sum==target)
+           {
+               return true;
+           }
+           else if(sum<target)
+           {
+               i++;
+           }
+           else
+           {
+               j--;
+           }
+        }
+
+        return false;
+    }
+};
