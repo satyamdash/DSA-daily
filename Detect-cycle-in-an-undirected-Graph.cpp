fdab22ca70814d84f59a681f36a92ce4ba80fc6b
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-28 23:26:58 +0530

    Cycle Detection

---
+++ Detect-cycle-in-an-undirected-Graph.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+  bool detect(int src, vector<vector<int>>& adj, vector<int>&vis) {
+      vis[src] = 1; 
+      // store <source node, parent node>
+      queue<pair<int,int>> q; 
+      q.push({src, -1}); 
+      // traverse until queue is not empty
+      while(!q.empty()) {
+          int node = q.front().first; 
+          int parent = q.front().second; 
+          q.pop(); 
+          
+          // go to all adjacent nodes
+          for(auto adjacentNode: adj[node]) {
+              // if adjacent node is unvisited
+              if(!vis[adjacentNode]) {
+                  vis[adjacentNode] = 1; 
+                  q.push({adjacentNode, node}); 
+              }
+              // if adjacent node is visited and is not it's own parent node
+              else if(parent != adjacentNode) {
+                  // yes it is a cycle
+                  return true; 
+              }
+          }
+      }
+      // there's no cycle
+      return false; 
+  }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(vector<vector<int>>& adj) {
+        int V=adj.size();
+        vector<int>vis(V,0);
+        for(int i = 0;i<V;i++) {
+            if(!vis[i]) {
+                if(detect(i, adj, vis)) return true; 
+            }
+        }
+        return false;
+        // Code here
+    }
+};
