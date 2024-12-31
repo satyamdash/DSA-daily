cae36c74644a9af299f8e7cba0287da20d6c9fa3
Satyam Gupta <satyam.bn6@gmail.com>
2024-12-31 15:08:01 +0530

    Recursive approach

--- Josephus-Problem.cpp
+++ Josephus-Problem.cpp
@@ -11,3 +11,25 @@ int predictTheWinner(int n, int k)
     return winner+1;
 
 }
+
+//Recursive approach
+
+class Solution {
+public:
+
+    int findindxwinner(int n,int k)
+    {
+        if(n==1) return 0;
+        int idx=findindxwinner(n-1,k);
+        idx=(idx+k)%n;
+
+        return idx;
+
+    }
+    int findTheWinner(int n, int k) 
+    {
+       int resultidx=findindxwinner(n,k);
+
+       return resultidx+1;
+    }
+};
