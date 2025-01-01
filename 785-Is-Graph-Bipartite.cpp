6446c9df04352b295b0b05c7b885cd2d312f2d6e
Satyam Gupta <satyam.bn6@gmail.com>
2025-01-01 20:27:20 +0530

    LC 785 dfs

--- 785-Is-Graph-Bipartite.cpp
+++ 785-Is-Graph-Bipartite.cpp
@@ -38,3 +38,49 @@ public:
         return true;
     }
 };
+
+
+
+//DFS
+
+class Solution {
+public:
+    bool DFS(int start,vector<int>&colors,vector<vector<int>>& graph)
+    {
+        for(auto node:graph[start])
+        {
+            if(colors[node]==-1)
+            {
+                colors[node]=!colors[start];
+                if(!DFS(node,colors,graph))
+                {
+                    return false;
+                }
+                
+            }
+            else if(colors[node]==colors[start])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int n=graph.size();
+        vector<int> colors(n,-1);
+
+        for(int begin=0;begin<n;begin++)
+        {
+            if(colors[begin]==-1)
+            {
+                colors[begin]=0;
+                 if(!DFS(begin,colors,graph))
+                 {
+                    return false;
+                 }
+            }
+        }
+        return true;
+    }
+};
