1dbbde8cf14482ecc0d7964b8cddfdff076288f4
Satyam Gupta <satyam.bn6@gmail.com>
2025-01-31 21:04:55 +0530

    solve Sudoku

---
+++ 31Jan-POTD-GFG.cpp
@@ -0,0 +1,58 @@
+class Solution {
+  public:
+  bool isSafe(vector<vector<int>> &mat,int row,int col,int num)
+  {
+      // Check if num exist in the row
+    for (int x = 0; x <= 8; x++)
+        if (mat[row][x] == num)
+            return false;
+
+    // Check if num exist in the col
+    for (int x = 0; x <= 8; x++)
+        if (mat[x][col] == num)
+            return false;
+
+    // Check if num exist in the 3x3 sub-matrix
+    int startRow = row - (row % 3), startCol = col - (col % 3);
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (mat[i + startRow][j + startCol] == num)
+                return false;
+
+    return true;
+  }
+    bool solveSudokuRec(vector<vector<int>> &mat,int row,int col){
+        int n = mat.size();
+
+        if (row == n - 1 && col == n)
+            return true;
+    
+
+        if (col == n) {
+            row++;
+            col = 0;
+        }
+    
+
+        if (mat[row][col] != 0)
+            return solveSudokuRec(mat, row, col + 1);
+    
+        for (int num = 1; num <= n; num++) {
+
+            if (isSafe(mat, row, col, num)) {
+                mat[row][col] = num;
+                if (solveSudokuRec(mat, row, col + 1))
+                    return true;
+                mat[row][col] = 0;
+            }
+        }
+      
+          return false;
+    }
+    
+    void solveSudoku(vector<vector<int>> &mat) {
+        
+         solveSudokuRec(mat, 0, 0);
+    }
+};
