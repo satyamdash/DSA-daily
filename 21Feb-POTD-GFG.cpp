0b43e2cba34eaa92ded9dbb478852047d7d26aea
Satyam Gupta <satyam.bn6@gmail.com>
2025-02-21 18:43:59 +0530

    Paranthesis checker

---
+++ 21Feb-POTD-GFG.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<char>st;
+        
+        for(auto ch:s)
+        {
+            if(ch=='{' || ch=='[' || ch=='(')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                else
+                {
+                    if(ch=='}' && st.top()=='{')
+                    {
+                        st.pop();
+                    }
+                    else if(ch==']' && st.top()=='[')
+                    {
+                        st.pop();
+                    }
+                    else if(ch==')' && st.top()=='(')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(ch);
+                    }
+                }
+            }
+           
+        }
+        return st.empty();
+    }
+};
