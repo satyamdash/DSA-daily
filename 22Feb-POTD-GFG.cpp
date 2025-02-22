223b8c952f42e60665005f0d0e4d99f479182ea6
Satyam Gupta <satyam.bn6@gmail.com>
2025-02-22 13:00:31 +0530

    Longest Valid parantheses

---
+++ 22Feb-POTD-GFG.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+    
+    int maxLength(string& s) {
+        stack<int> st;
+
+    // Push -1 as the initial index to 
+      // handle the edge case
+    st.push(-1);
+    int maxLen = 0;
+
+    // Traverse the string
+    for (int i = 0; i < s.length(); i++) {
+        
+        // If we encounter an opening parenthesis,
+          // push its index
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            
+            // If we encounter a closing parenthesis,
+              // pop the stack
+            st.pop();
+
+            // If stack is empty, push the current index 
+            // as a base for the next valid substring
+            if (st.empty()) {
+                st.push(i);
+            } else {
+                
+                // Update maxLength with the current length 
+                // of the valid parentheses substring
+                maxLen = max(maxLen, i - st.top());
+            }
+        }
+    }
+
+    return maxLen;
+    }
+};
