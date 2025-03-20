18210d805b44dfa0e9cc2e771a63689a564012f2
Satyam Gupta <satyam.bn6@gmail.com>
2025-03-20 19:25:34 +0530

    Stock Buy and Sell – Max 2 Transactions Allowed

---
+++ 20Mar-POTD-GFG.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int dp[100001][3][2];
+     int f(int idx,vector<int>& prices, int k,int buy)
+    {
+        if (idx >= prices.size() || k <= 0) return 0;
+        if(dp[idx][k][buy]!=-1) return dp[idx][k][buy];
+        int profit = 0;
+
+        if (buy) {
+            // Either buy the stock or skip
+            profit = max(-prices[idx] + f(idx + 1, prices, k, 0), f(idx + 1, prices, k, 1));
+        } else {
+            // Either sell the stock (reducing k) or skip
+            profit = max(prices[idx] + f(idx + 1, prices, k - 1, 1), f(idx + 1, prices, k, 0));
+        }
+        
+        return dp[idx][k][buy] =profit;
+    }
+    int maxProfit(vector<int> &prices) {
+        // code here
+        int n=prices.size();
+        memset(dp, -1, sizeof(dp)); 
+        return f(0,prices,2,1);
+    }
+};
