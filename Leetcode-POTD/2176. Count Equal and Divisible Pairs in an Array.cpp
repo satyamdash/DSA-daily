85c3cae7ea6517167e404735c63ffbaa158ce97e
Satyam Gupta <satyam.bn6@gmail.com>
2025-04-17 20:35:27 +0530

    LC 2176 2nd intution

--- Leetcode-POTD/2176. Count Equal and Divisible Pairs in an Array.cpp
+++ Leetcode-POTD/2176. Count Equal and Divisible Pairs in an Array.cpp
@@ -19,3 +19,30 @@ public:
 };
 
 //2nd Intution
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n=nums.size();
+        int count=0;
+        unordered_map<int,vector<int>>mpp;
+
+        for(int i=0;i<n;i++)
+        {
+           mpp[nums[i]].push_back(i);
+        }
+
+        for (auto& [val, indices] : mpp) {
+        int sz = indices.size();
+        // O(sz^2): check all pairs in that group
+        for (int i = 0; i < sz; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                if ((indices[i] * indices[j]) % k == 0)
+                    count++;
+        }
+    }
+}
+
+        return count;
+    }
+};
